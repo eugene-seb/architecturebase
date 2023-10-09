50a91ecd3b7fd0c07ba38ac02e59f6389aeaa5fc
eugene-seb <etoundisebastien@gmail.com>
2023-10-09 13:00:18 +0200

    Part 4. Implement Rest Communication Client

--- client/clienteRest.js
+++ client/clienteRest.js
@@ -24,10 +24,10 @@ function ClienteRest() {
             type: "GET",
             url: "/obtenerUsuarios",
             success: function (data) {
-                if (data.nick != -1) {
-                    console.log("Usuario " + data.usuarios + " ha sido registrado");
+                if (data != -1) {
+                    console.log("Data " + data );
                 } else {
-                    console.log("El nick ya está ocupado");
+                    console.log("No user is register yet.");
                 }
             },
             error: function (xhr, textStatus, errorThrown) {
@@ -38,5 +38,61 @@ function ClienteRest() {
         });
     };
 
+    this.activeUser = function (nick) {
+        $.ajax({
+            type: "GET",
+            url: "/activeUser/" + nick,
+            success: function (data) {
+                if (data) { // Should be either true or false
+                    console.log("the user " + nick + " is active");
+                } else {
+                    console.log("the user " + nick + " is not active");
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log("Status: " + textStatus);
+                console.log("Error: " + errorThrown);
+            },
+            contentType: "application/json",
+        });
+    };
+
+    this.deleteUser = function (nick) {
+        $.ajax({
+            type: "GET",
+            url: "/deleteUser/" + nick,
+            success: function (data) {
+                if (data) { // Should be either true or false
+                    console.log("the user " + nick + " has been delete");
+                } else {
+                    console.log("the user " + nick + " is not active to be delete");
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log("Status: " + textStatus);
+                console.log("Error: " + errorThrown);
+            },
+            contentType: "application/json",
+        });
+    };
+
+    this.countUsers = function () {
+        $.ajax({
+            type: "GET",
+            url: "/countUsers",
+            success: function (data) {
+                if (data != -1) {
+                    console.log("Number of users " + data.num );
+                } else {
+                    console.log("No user is register yet.");
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log("Status: " + textStatus);
+                console.log("Error: " + errorThrown);
+            },
+            contentType: "application/json",
+        });
+    };
 
 }
