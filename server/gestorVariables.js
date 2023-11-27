307994aa9e8f9079ad038652baacb7ca69b2aac1
eugene-seb <etoundisebastien@gmail.com>
2023-11-27 09:18:12 +0100

    Sprint 3: Manage Sensitive Information stuck a some point

---
+++ server/gestorVariables.js
@@ -0,0 +1,17 @@
+const { SecretManagerServiceClient } = require("@google-cloud/secret-manager");
+const client = new SecretManagerServiceClient();
+
+async function accessCLAVECORREO() {
+    const name = "projects/590521076034/secrets/azertytreize/versions/1";
+    const [version] = await client.accessSecretVersion({
+        name: name,
+    });
+    const datos = version.payload.data.toString("utf8");
+    return datos;
+}
+module.exports.obtenerOptions = async function (callback) {
+    let options = { user: "", pass: "" };
+    let user = await accessCLAVECORREO();
+    options.pass = pass;
+    callback(options);
+};
